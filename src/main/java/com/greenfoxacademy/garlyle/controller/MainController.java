48e9960b8658a4e7ee22218e0b2ea413bfda1c17
Milan Kovacs <kovacsmilan85@gmail.com>
2017-05-17 21:12:41 +0200

    change username

--- src/main/java/com/greenfoxacademy/garlyle/controller/MainController.java
+++ src/main/java/com/greenfoxacademy/garlyle/controller/MainController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
@@ -27,10 +28,12 @@ public class MainController {
   }
 
   @RequestMapping("/")
-  public String index(@RequestParam(required = false) String username) {
+  public String index(@RequestParam(required = false) String username, Model model) {
     if (username != null) {
       if (userRepository.findByUsername(username).isEmpty()) {
         return "redirect:/register";
+      } else {
+        model.addAttribute("user", userRepository.findByUsername(username).get(0));
       }
     }
     return "index";
@@ -42,16 +45,30 @@ public class MainController {
   }
 
   @PostMapping("/newuser")
-  public String addNewUser(String username, Model model) {
+  public String addNewUser(String username, RedirectAttributes attr, Model model) {
     if (username.isEmpty()) {
       model.addAttribute("error", "The username field is empty");
       return "register";
     } else {
-      List<User> existingUsers = userRepository.findByUsername(username);
       if (userRepository.findByUsername(username).isEmpty()) {
         User user = new User(username);
         userRepository.save(user);
+        attr.addAttribute("username", user.getUsername());
+      } else {
+        attr.addAttribute("username", username);
       }
+      return "redirect:/";
+    }
+  }
+
+  @PostMapping("/changeuser")
+  public String changeUser(@ModelAttribute User user, RedirectAttributes attr, Model model) {
+    if (user.getUsername().isEmpty()) {
+      model.addAttribute("error", "The username field is empty");
+      return "index";
+    } else {
+      userRepository.save(user);
+      attr.addAttribute("username", user.getUsername());
       return "redirect:/";
     }
   }
